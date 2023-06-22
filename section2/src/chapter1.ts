abde703f65efc46c499b6eeb3917fb3c58381240
Gull 2365 <jych2365@gmail.com>
2023-06-22 12:00:17 +0900

    feat: 원시타입과 리터럴 타입

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,33 @@
+// number
+let num1: number = 123; // :number 타입 주석이라 부름
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity; // 숫자의 양의 무한대
+let num6: number = -Infinity; //음의 무한대
+let num7: number = -NaN; //Not a Number
+
+// string
+let str1: string = "hello";
+let str2: string = "hello";
+let str3: string = `hello`;
+let str4: string = `hello ${num1}`;
+
+// boolean
+let bool1: boolean = true;
+let boo2: boolean = false;
+
+//null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+// 리터럴 타입
+// 리터럴 -> 값
+let NumA: 10 = 10;
+NumA = 10;
+
+let strA: "Hello" = "Hello";
+
+let boolA: true = true;
