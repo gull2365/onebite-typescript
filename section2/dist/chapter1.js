8fad4a3cedaa740cd11db857636aab74aaed01fd
Gull 2365 <jych2365@gmail.com>
2023-06-25 23:19:34 +0900

    feat: 배열과 튜플

---
+++ section2/dist/chapter1.js
@@ -0,0 +1,27 @@
+// number
+let num1 = 123; // :number 타입 주석이라 부름
+let num2 = -123;
+let num3 = 0.123;
+let num4 = -0.123;
+let num5 = Infinity; // 숫자의 양의 무한대
+let num6 = -Infinity; //음의 무한대
+let num7 = -NaN; //Not a Number
+// string
+let str1 = "hello";
+let str2 = "hello";
+let str3 = `hello`;
+let str4 = `hello ${num1}`;
+// boolean
+let bool1 = true;
+let boo2 = false;
+//null
+let null1 = null;
+// undefined
+let unde1 = undefined;
+// 리터럴 타입
+// 리터럴 -> 값
+let NumA = 10;
+NumA = 10;
+let strA = "Hello";
+let boolA = true;
+export {};
