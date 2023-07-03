587cc55e95b72d1287687a9821bb058c3ed5dcad
Gull 2365 <jych2365@gmail.com>
2023-07-03 22:33:40 +0900

    feat: 서로소 유니온 타입

---
+++ section3/src/chapter8.ts
@@ -0,0 +1,119 @@
+/**
+ * 서로소 유니온 타입
+ * 교집합이 없는 타입들로만 만든 유니온 타입을 말함 (두 타입 간의 공통점이 하나도 없는 타입)
+ */
+
+type Admin = {
+  tag: "ADMIN";
+  name: string;
+  kickCount: number;
+};
+
+type Member = {
+  tag: "MEMBER";
+  name: string;
+  point: number;
+};
+
+type Guest = {
+  tag: "GUEST";
+  name: string;
+  visitCount: number;
+};
+
+type User = Admin | Member | Guest; // 서로소 유니온 타입
+
+// Admin -> {name}님 현재까지 {kickCount}명 강퇴 했습니다.
+// Member -> {name }님 현재까지 {point}모았습니다.
+// Guest -> {name} 님 현재까지 {visitCount} 오셨습니다.
+function login(user: User) {
+  switch (user.tag) {
+    case "ADMIN": {
+      console.log(`${user.name}님 현재까지 ${user.kickCount}명 강퇴 했습니다.`);
+      break;
+    }
+    case "MEMBER": {
+      console.log(`${user.name}님 현재까지 ${user.point}모았습니다.`);
+      break;
+    }
+    case "GUEST": {
+      console.log(`${user.name} 님 현재까지 ${user.visitCount} 오셨습니다.`);
+      break;
+    }
+  }
+
+  //   if (user.tag === "ADMIN") {
+  //     console.log(`${user.name}님 현재까지 ${user.kickCount}명 강퇴 했습니다.`);
+  //     // admin 타입
+  //   } else if (user.tag === "MEMBER") {
+  //     console.log(`${user.name}님 현재까지 ${user.point}모았습니다.`);
+  //     //member 타입
+  //   } else {
+  //     //guest 타입
+  //     console.log(`${user.name} 님 현재까지 ${user.visitCount} 오셨습니다.`);
+  //   }
+}
+/**
+ * 복습겸 한가지 더 사례
+ */
+
+// 비동기 작업의 결과를 처리하는 객체
+
+type LoadingTask = {
+  state: "LOADING";
+};
+
+type FailedTask = {
+  state: "FAILED";
+  error: {
+    message: string;
+  };
+};
+
+type SuccessTask = {
+  state: "SUCCESS";
+  response: {
+    data: string;
+  };
+};
+
+type AsyncTask = LoadingTask | FailedTask | SuccessTask;
+
+// 로딩중 -> 콘솔에 로딩중 출력
+// 실패 -> 실패 : 에러 메세지를 출력
+// 성공 -> 성공 : 데이터를 출력
+
+function processResult(task: AsyncTask) {
+  switch (task.state) {
+    case "LOADING": {
+      console.log("로딩중");
+      break;
+    }
+    case "FAILED": {
+      console.log(`에러 발생 : ${task.error.message}`);
+      break;
+    }
+    case "SUCCESS": {
+      console.log(`성공: ${task.response.data}`);
+      break;
+    }
+  }
+}
+
+const loading: AsyncTask = {
+  state: "LOADING",
+};
+
+const failed: AsyncTask = {
+  state: "FAILED",
+  error: {
+    message: "오류 발생원인은 ~",
+  },
+};
+
+const success: AsyncTask = {
+  state: "SUCCESS",
+  response: {
+    data: "데이터~~",
+  },
+};
