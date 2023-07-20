838fa95e8e884f24ef853c9ff0aaf405d7684567
Gull 2365 <jych2365@gmail.com>
2023-07-21 03:45:30 +0900

    feat: 인덱스드 엑세스 타입

---
+++ section8/src/chapter0.ts
@@ -0,0 +1,36 @@
+/**
+ * 인덱스드 엑세스 타입
+ */
+
+type PostList = {
+  title: string;
+  content: string;
+  author: {
+    id: number;
+    name: string;
+    age: number;
+  };
+}[];
+
+function printAuthorInfo(author: PostList[number]["author"]) {
+  console.log(`${author.name}-${author.id}`);
+}
+const post: PostList[0] = {
+  title: "게시글 제목",
+  content: "게시글 본문",
+  author: {
+    id: 1,
+    name: "정요찬",
+    age: 19,
+  },
+};
+
+printAuthorInfo(post.author);
+
+type Tup = [number, string, boolean];
+
+type Tup0 = Tup[0];
+type Tup1 = Tup[1];
+type Tup2 = Tup[2]; // Tuple에 number가 넘어가면 값을 못 뽑음
+
+type TupNum = Tup[number]; // 세 타입의 유니온타입 최적의 타입을 뽑는 방법
